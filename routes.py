94c12be9d659512aea34d823ded7dd1ea30bc00d
kent-wong <w02682@gmail.com>
2019-06-07 23:02:56 +0800

    update

--- routes.py
+++ routes.py
@@ -11,8 +11,8 @@ from PIL import Image
 @app.route('/')
 @app.route('/home')
 def home():
-    page =request.args.get('page', 1, type=int)
-    posts = Post.query.paginate(page=page, per_page=5)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 @app.route('/about')
@@ -146,3 +146,10 @@ def delete_post(post_id):
     db.session.commit()
     flash('删除成功', 'success')
     return redirect(url_for('home'))
+
+@app.route('/user/<string:username>')
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
